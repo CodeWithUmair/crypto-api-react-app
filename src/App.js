3acb6c4f4984aef48c640ec07116a9f7f9c603c8
Umair <codewithumair867@gmail.com>
2022-07-26 04:33:00 +0500

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
 import axios from "axios";
+import { Routes, Route } from "react-router-dom";
 import Coins from "./components/Coins";
 import Coin from "./routes/Coin";
+import Navbar from "./components/Navbar";
 
-const App = () => {
+function App() {
   const [coins, setCoins] = useState([]);
 
-  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false`;
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false";
 
   useEffect(() => {
     axios
       .get(url)
-      .then((res) => {
-        setCoins(res.data);
-        console.log(res.data[0]);
+      .then((response) => {
+        setCoins(response.data);
+        // console.log(response.data[0])
       })
-      .catch((error) => console.log(error));
-  }, [url]);
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -30,6 +34,6 @@ const App = () => {
       </Routes>
     </>
   );
-};
+}
 
 export default App;
