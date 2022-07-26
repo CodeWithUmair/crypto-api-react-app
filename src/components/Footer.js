5e65a5bd95bb09c7f2ff6801b88f4736a1a3980b
Umair <codewithumair867@gmail.com>
2022-07-26 06:03:17 +0500

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,51 +6,24 @@ const Footer = () => {
     <>
       <footer>
         <span>
-          Created By
+          Powered by &nbsp;
           <a
             href="https://www.freecodecamp.org/codewithumair"
             target="_blank"
             rel="noreferrer"
           >
-            Umair
+            Umair&nbsp;
           </a>
           | <span class="far fa-copyright"></span> 2022 All rights reserved.
         </span>
-        <br />
         <span>
-          <a
-            href="https://www.linkedin.com/in/umair-amir-30b32721a/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="/icons8-linkedin-2.gif"
-              alt="linkedin"
-              width="36px"
-              height="36px"
-              class="footer-links"
-            />
-          </a>
-          <a
-            href="https://twitter.com/Muhamma19512164"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="/icons8-twitter.gif"
-              alt="linkedin"
-              width="36px"
-              height="36px"
-              class="footer-links"
-            />
-          </a>
           <a
             href="https://github.com/CodeWithUmair"
             target="_blank"
             rel="noreferrer"
           >
             <img
-              src="/icons8-github.gif"
+              src="./github.png"
               alt="github"
               width="36px"
               height="36px"
