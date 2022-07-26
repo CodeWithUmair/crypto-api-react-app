ecf09e1ea9bb9e4ba05eee2f4ea11957e2f6189c
Umair <codewithumair867@gmail.com>
2022-07-27 02:35:19 +0500

    rm

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,11 @@ import React from "react";
 import { FaCoins } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-// import { moment } from "moment.js";
 
 const Navbar = () => {
   return (
     <Link to="/">
       <div className="navbar">
-        {/* <b>{moment().format("MMMM Do YYYY, h:mm:ss a")}</b> */}
         <FaCoins className="icon" />
         <h1>
           Crypto <span className="span-color"> Coins</span>
