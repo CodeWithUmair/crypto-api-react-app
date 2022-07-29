604eaa1f753f614f9931ceffda10783f46922b69
Umair <codewithumair867@gmail.com>
2022-07-30 03:49:36 +0500

    add time below navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import React from "react";
 import { FaCoins } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -12,6 +13,7 @@ const Navbar = () => {
           Crypto <span className="span-color"> Coins</span>
         </h1>
       </div>
+      <h5>{moment().format("MMMM Do YYYY, h:mm:ss a")}</h5>
     </Link>
   );
 };
