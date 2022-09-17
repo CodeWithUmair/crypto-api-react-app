5de7dfa7b145e69629fad77726e6eedfe5b7d00d
Umair <codewithumair867@gmail.com>
2022-09-18 03:41:27 +0500

    current screen width

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CoinItem from "./CoinItem";
 import moment from "moment";
-
 import { Link } from "react-router-dom";
 import "./Coins.css";
 import Coin from "./../routes/Coin";
 
 const Coins = ({ coins }) => {
+  const [deviceWidth, setDeviceWidth] = useState(window.screen.width);
+
+  const currentScreenWidth = () => {
+    setDeviceWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", currentScreenWidth);
+  });
+
   return (
     <>
       <div className="container">
-        <h5>{moment().format("MMMM Do YYYY, h:mm a")}</h5>
+        <h4>
+          Date: <h5>{moment().format("MMMM Do YYYY, h:mm a")}</h5>
+        </h4>
+        <h4>
+          Current Screen:
+          <h5>
+            Width <span> {deviceWidth}px</span>
+          </h5>
+        </h4>
         <div>
           <div className="heading">
             <p>#</p>
